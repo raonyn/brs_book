2c0daa6d71f8f1b4c7c462db2bfd048c640427a0
raonyn <95270150+raonyn@users.noreply.github.com>
2022-07-28 15:53:41 +0900

    calendar app add

--- brs_book/settings.py
+++ brs_book/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'brs_app.apps.BrsAppConfig',
+    'brs_cal.apps.BrsCalConfig',
     'account.apps.AccountConfig',
  #   'book.apps.BookConfig',
     
